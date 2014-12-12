de92444beed3bdf925ad9154da16c6eb76e19a3b
Brennan Brown <bbrown17@punahou.edu>
2014-12-11 14:52:56 -1000

    Final Blue Cursor Commit
    Took out glitchy off-screen error sound

--- NowrapCursor.ino
+++ NowrapCursor.ino
@@ -43,31 +43,23 @@ void shift() // taken from Maze_Game
   if (Button_Right)
   {
     if (xcoord < 7)
-      xcoord = xcoord + 1; // indented because belongs to inner if statement; could also say xcoord++
-    if (xcoord == 7)
-      Tone_Start (ToneC3,100); 
+      xcoord = xcoord + 1; // could also say xcoord++
   }    
   if (Button_Left)
   {
     if (xcoord > 0)
       xcoord = xcoord - 1;
-    if (xcoord == 0)
-      Tone_Start (ToneC3,100);  
   }    
     if (Button_Up)
   {
     if (ycoord < 7)
       ycoord = ycoord + 1;
-    if (ycoord == 7)
-      Tone_Start (ToneC3,100); // play error sound when go off screen
   }    
   
   if (Button_Down)
   {
     if (ycoord > 0)
       ycoord = ycoord - 1;
-    if (ycoord == 0)
-      Tone_Start (ToneC3,100); // play error sound when go off screen  
   } 
 
   
